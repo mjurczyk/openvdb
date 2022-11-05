82ad76d696d430a47646f39055606e19cead44a1
mjurczyk <thelairmail@gmail.com>
2022-11-05 16:43:01 +0100

    Adjust package bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,57 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import uglify from '@lopatnov/rollup-plugin-uglify';
+import copy from 'rollup-plugin-copy';
+import externalPeerDependencies from 'rollup-plugin-peer-deps-external';
+import cleanUpDir from 'rollup-plugin-delete';
 
 export default [
   {
-    // Dist
+    // NOTE ESM Core
     input: './src/openvdb/index.js',
     output: {
-      file: './build/openvdb.js',
+      file: './build/index.js',
       format: 'esm',
     },
-    plugins: [nodeResolve(), uglify()],
+    plugins: [
+      cleanUpDir({ targets: './build' }),
+      externalPeerDependencies(),
+      nodeResolve(),
+      uglify(),
+      copy({
+        targets: [
+          {
+            src: './package.json',
+            dest: './build/',
+          },
+        ],
+      }),
+    ],
   },
   {
-    // NOTE Tools
+    // NOTE ESM Three.js
     input: './src/openvdb/three/index.js',
     output: {
-      file: './build/openvdb-three.js',
+      file: './build/three/index.js',
       format: 'esm',
     },
-    plugins: [nodeResolve(), uglify()],
+    plugins: [externalPeerDependencies(), nodeResolve(), uglify()],
+  },
+  {
+    // NOTE CJS Core
+    input: './src/openvdb/index.js',
+    output: {
+      file: './build/index.cjs.js',
+      format: 'cjs',
+    },
+    plugins: [externalPeerDependencies(), nodeResolve(), uglify()],
+  },
+  {
+    // NOTE CJS Three.js
+    input: './src/openvdb/three/index.js',
+    output: {
+      file: './build/three/index.cjs.js',
+      format: 'cjs',
+    },
+    plugins: [externalPeerDependencies(), nodeResolve(), uglify()],
   },
 ];
